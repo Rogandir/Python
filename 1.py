6fae4dc207286dcacbf3792cfb06ddfc4f59bd52
Rogandir <therogerr@gmail.com>
2014-03-09 17:39:45 +0200

    Now shows correct directory and can read a file

--- 1.py
+++ 1.py
@@ -2,7 +2,7 @@ import sys
 import os
 
 def get_dir(argv):
-	defdir = "E:\\VU\Python\Files"
+	defdir = "E:\\VU\Python\Files\\"
 	if len(argv) > 1:
 		return argv[1]
 	else:
@@ -12,6 +12,15 @@ def get_files(listdir):
 	dirs = os.listdir(direct)
 	return dirs
 	
+def get_dirlist(file_list, directory):
+	dirlist=[]
+	for file in file_list:
+		dirlist.append(directory + file)
+	return dirlist
+	
 direct = get_dir(sys.argv)
 files = get_files(os.listdir)
+directory = get_dirlist(files, direct)
+fd = open(directory[0])
+print (fd.read())
 
