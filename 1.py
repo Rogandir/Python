6dd911c760ebce6c6079cbb2066dc8bff68b1806
Rogandir <therogerr@gmail.com>
2014-03-09 18:52:06 +0200

    Counts chars too!

--- 1.py
+++ 1.py
@@ -18,9 +18,36 @@ def get_dirlist(file_list, directory):
 		dirlist.append(directory + file)
 	return dirlist
 	
+def get_chars(file):
+	fd = open (file)
+	data = fd.read()
+	charlist={}
+	for char in data:
+		if char not in charlist:
+			charlist[char] = 1
+		else:
+			charlist[char] += 1
+	#for c,n in charlist.items():
+	#	print (c, n)
+	return file
+	
+def get_words(file):
+	fd = open (file)
+	wordlist={}
+	for word in fd.read().split():
+		if word not in wordlist:
+			wordlist[word] = 1
+		else:
+			wordlist[word] += 1
+			
+	#for w,n in wordlist.items():
+	#	print (w, n)	
+	
 direct = get_dir(sys.argv)
 files = get_files(os.listdir)
 directory = get_dirlist(files, direct)
-fd = open(directory[0])
-print (fd.read())
+for file in directory:
+	get_chars(file)
+	get_words(file)
+
 
